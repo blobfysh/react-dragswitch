bdcd1d17fb9869a75d13df61e62100633e4f74cc
blobfysh <47480729+blobfysh@users.noreply.github.com>
2020-12-21 23:17:10 -0500

    Updates

---
+++ precache-manifest.e6918b435c4dd5d5ccc7f83a2c929c61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302efd13fbc732385e8f1d43c2ae184d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bce98b57253488c49abc",
+    "url": "./static/css/main.075a33ab.chunk.css"
+  },
+  {
+    "revision": "345f175ca7ac689dd7fc",
+    "url": "./static/js/2.15cc3e52.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.15cc3e52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce98b57253488c49abc",
+    "url": "./static/js/main.b37847ee.chunk.js"
+  },
+  {
+    "revision": "1e8aa83bf274e0ba4f41",
+    "url": "./static/js/runtime-main.0bc86aa9.js"
+  }
+]);
